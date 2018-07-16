a7ee0d755abcc2be4c3f9c71e3d537d19f804f53
Dave Methvin <david.w.methvin@omb.eop.gov>
2018-07-16 13:16:30 -0400

    Single-file sample without any external dependencies

--- js/config/form.js
+++ js/config/form.js
@@ -1,53 +1,147 @@
 import Introduction from '../components/Introduction.jsx';
-import fullName from '../definitions/fullName';
-
-import fullNameUI from 'us-forms-system/lib/js/definitions/fullName';
 
 const formConfig = {
-  title: 'Form',
-  subTitle: 'Test',
+  title: 'Kitchen Sink demo for US Forms System',
+  subTitle: 'Form USFS-KS1',
+
   formId: '',
   urlPrefix: '/',
-  trackingPrefix: 'form-',
+  trackingPrefix: '',
+
   transformForSubmit: '',
   submitUrl: '',
+  consoleSubmit: true,
+
   introduction: Introduction,
   confirmation: '',
+  footer: '',
+
+  // References these definitions in a `schema` using '#/ref:'
   defaultDefinitions: {
-    fullName
   },
+
   chapters: {
     firstSection: {
-      title: 'First Section',
+      title: 'Name and Contact Information',
       pages: {
-        firstPage: {
-          path: 'first-section/first-page',
-          title: 'First Page',
-          uiSchema: {
-            fullName: fullNameUI
-          },
+        contactName: {
+          path: 'contact/yourname',
+          title: 'Your Name',
           schema: {
             type: 'object',
             properties: {
-              fullName
+              fullName: {
+                type: 'object',
+                properties: {
+                  first: { type: 'string', maxLength: 30 },
+                  middle: { type: 'string', maxLength: 30 },
+                  last: { type: 'string', maxLength: 30 },
+                  suffix: {
+                    type: 'string',
+                    'enum': [ 'Jr.', 'Sr.', 'II', 'III', 'IV' ]
+                  },
+                },
+                required: [ 'first', 'last' ]
+              },
+              phone: {
+                type: 'string',
+                pattern: '^[\\d.-()\\s]+$',
+                minLength: 10,
+                maxLength: 20
+              }
+            },
+            required: [ 'fullName' ]
+          },
+          uiSchema: {
+            fullName: {
+              first: { 'ui:title': 'First name' },
+              last: { 'ui:title': 'Last name' },
+              middle: { 'ui:title': 'Middle name' },
+              suffix: {
+                'ui:title': 'Suffix',
+                'ui:options': {
+                  widgetClassNames: 'form-select-medium'
+                }
+              }
+            },
+            phone: {
+              'ui:title': 'Phone',
+              'ui:errorMessages': {
+                pattern: 'Please enter a valid phone number'
+              }
             }
           }
         },
-        secondPage: {
-          path: 'first-section/second-page',
-          title: 'Second Page',
-          uiSchema: {},
+        contactSocial: {
+          path: 'contact/socialinfo',
+          title: 'Social Information',
           schema: {
             type: 'object',
-            properties: {}
+            properties: {
+              email: {
+                type: 'string',
+                pattern: '.*@.*',
+                minLength: 7,
+                maxLength: 30
+              },
+              github: {
+                type: 'string'
+              },
+              twitter: {
+                type: 'string'
+              }
+            },
+            required: [ 'email' ]
+          },
+          uiSchema: {
+            email: {
+              'ui:title': 'Email',
+              'ui:errorMessages': {
+                pattern: 'Please enter a valid email address'
+              }
+            },
+            github: { 'ui:title': 'Github' },
+            twitter: { 'ui:title': 'Twitter'}
           }
         }
       }
     },
     secondSection: {
-      title: 'Second Section',
+      title: 'Mailing Address',
       pages: {
-
+        mailAddress: {
+          path: 'contact/mailing-address',
+          title: 'Address, City, and State',
+          schema: {
+            type: 'object',
+            required: [ 'addr1', 'city', 'state', 'zip' ],
+            properties: {
+              addr1: { type: 'string', maxLength: 50 },
+              addr2: { type: 'string', maxLength: 50 },
+              city: { type: 'string', maxLength: 50 },
+              state: {
+                type: 'string',
+                maxLength: 2,
+                'enum': [
+                  'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC',
+                  'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS',
+                  'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO',
+                  'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP',
+                  'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN',
+                  'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'
+                 ]
+              },
+              zip: { type: 'string', maxLength: 10 },
+            }
+          },
+          uiSchema: {
+            addr1: { 'ui:title': 'Address Line 1' },
+            addr2: { 'ui:title': 'Address Line 2' },
+            city: { 'ui:title': 'City' },
+            state: { 'ui:title': 'State' },
+            Zip: { 'ui:title': 'Zip' }
+          }
+        }
       }
     }
   }
