5ed4b1e780d6c7f39b4e1c1eb488c44e2f990c62
Dave Methvin <David.W.Methvin@omb.eop.gov>
2018-07-19 15:50:45 -0400

    Use better way of generating sourcemaps

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,7 @@ const merge = require('webpack-merge');
 
 module.exports = merge(require('./webpack.common.js'), {
   mode: 'development',
-  devtool: 'eval-source-map',
+  devtool: 'cheap-module-source-map',
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     historyApiFallback: true,
